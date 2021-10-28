9a1ff90ae7dcdc0366be85652acc8cc1ae5f136d
brochhagen <thomasbrochhagen@gmail.com>
2021-10-28 18:10:55 +0200

    sketch + render in knitr

--- material/2022q3/session01.R
+++ material/2022q3/session01.R
@@ -1,34 +1,84 @@
-## -----------------------------------------------------------------------------
-print('Welcome to the Quantitative Methods Series')
+## -----------------------------------------------------------------------------------------
+heights1 <- c(170,178,162,166,183,185) #a vector of heights in cms
 
-#A demonstration of R code (to see that the code is running)
-x <- 2 #this assigns the value '2' to variable 'x'
-y <- 5 #this assigns the value '5' to variable 5
-
-#Running this box of code will print the result of these operations:
-x+y 
-x*y
-x**y
+mean(heights)
 
 
-## -----------------------------------------------------------------------------
-df_a <- read.csv('df_session01a.csv')
-head(df_a)
-str(df_a)
+## -----------------------------------------------------------------------------------------
+sort(heights) #not necessary to compute median, just for illustration
+median(heights)
+
+sort(heights2) #not necessary to compute median, just for illustration
+median(heights2)
 
 
-## -----------------------------------------------------------------------------
-df_b <- read.csv('df_session01b.csv')
-head(df_b)
-str(df_b)
+## -----------------------------------------------------------------------------------------
+
+heights3 <- c(170,178,162,166,183,185,188,300) #a vector of heights in cms
+mean(heights3)
+median(heights3)
 
 
-## -----------------------------------------------------------------------------
-df_b <- read.csv('df_session01b.csv',sep='*')
-head(df_b)
-str(df_b)
+## -----------------------------------------------------------------------------------------
+#Let's automatize and let the computer create some data for us
+
+#We will simulate reaction times (in milliseconds)
+#Starting small, generating just 10 observations and telling R that the central tendency is 150 ms, with 20 ms of standard deviation
+
+sim_reaction_times <- rnorm(n = 10, mean = 150, sd = 20) #sample 1000 times from a Normal distribution with mean 150 and sd of 20
+print(sim_reaction_times) #look at what reaction times we sampled
+plot(sim_reaction_times) #visualize the reaction times we sampled
+
+mean(sim_reaction_times) #calculate their mean
+median(sim_reaction_times) #calculate their median
 
 
-## -----------------------------------------------------------------------------
+#The same thing, but now with more observations sampled
+
+sim_reaction_times <- rnorm(n = 1000, mean = 150, sd = 20) #sample 1000 times from a Normal distribution with mean 150 and sd of 20
+print(sim_reaction_times) #look at what reaction times we sampled
+plot(sim_reaction_times) #visualize the reaction times we sampled
+
+mean(sim_reaction_times) #calculate their mean
+median(sim_reaction_times) #calculate their median
+
+plot(density(sim_reaction_times))
+
+#Changing the generating distribution
+sim_reaction_times_g <- rgamma(n = 1000, shape=1, scale=150) #sample 1000 times from a Gamma distribution with mean 150 and sd of 20
+
+plot(sim_reaction_times_g)
+
+mean(sim_reaction_times_g)
+median(sim_reaction_times_g)
+
+plot(density(sim_reaction_times_g))
+
+
+## -----------------------------------------------------------------------------------------
+heights
+var(heights)
+sd(heights) #the same as sqrt(var(heights))
+
+#Unpacking variance and standard deviation
+x_bar <- mean(heights) #this is the mean of the heights
+print(x_bar)
+
+dev_heights <- heights - x_bar #deviations of each height from the mean
+print(dev_heights)
+
+squared_dev_heights <- devs_heights**2 #squared deviations from the mean
+print(squared_dev_heights)
+
+sum_squared_dev_heights <- sum(squared_dev_heights) #sum of the squared deviations
+print(sum_squared_dev_heights)
+
+var_heights <- ( 1 / (length(heights)-1) ) * sum_squared_dev_heights #divide by number of observations - 1
+print(var_heights)
+
+print(sqrt(var_heights)) #square root of the variance = std
+
+
+## -----------------------------------------------------------------------------------------
 sessionInfo()
 
